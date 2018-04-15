f4ef699170f90f8cd4bc8fd88c1266cb59669fd5
Cole Sellers <csellers18@yahoo.com>
2018-04-15 16:43:27 -0400

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,35 @@
+/*Var variable */
+var name ='john doe';
+console.log(name);
+name = 'steve smith';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting = 'hello';
+console.log(greeting);
+
+var firstname = 'John'  //Camel Case
+var first_name = 'Sara' //UnderScore
+var FirstName = 'Tom' //Pascel case
+var firstname; //dont do
+
+
+/*Let variable */
+
+let name ='john doe';
+console.log(name);
+name = 'steve smith';
+console.log(name);
+
+/*Const variable */
+const name ="John";
+console.log(name);
+
+const person ={
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
